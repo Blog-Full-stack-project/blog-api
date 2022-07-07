a78db7878460fb0671f4426f7deb253a3671f09e
Luiz Davi <luizd4398@gmail.com>
2022-07-07 10:34:43 -0300

    Revert "Notifications"

--- app/graphql/types/like_type.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Types
-  class LikeType < Types::BaseObject
-    field :id, ID, null: false
-    field :post_id, Integer
-    field :user_id, Integer
-  end
-end
